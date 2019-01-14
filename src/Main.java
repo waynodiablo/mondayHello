9c5abe2b262346d8348b9dce315d94e987105c4c
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2019-01-14 12:36:36 +0000

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        greeting();
+    }
+
+    private static void greeting() {
+        String name = "Hello Wayne";
+        int len = name.length();
+        System.out.println(name);
+        System.out.println(len);
+    }
+
+
+    }
---
+++ src/MainTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.BitSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+//
+//    @Test
+//    void greeting() {
+//        assert name.length() == 11 : "Length of greeting is too long!";
+//        assertTrue(8 <= len && len <= 15);
+//    }
+
+
+}
